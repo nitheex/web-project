212a4ac5763609a2a6a0c87d3060deb625f42e59
NITHEESHKUMAR M <nitheesh638331@gmail.com>
2026-02-21 14:25:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToRequest() {
+    window.location.href = "request.html";
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("✅ Your request has been submitted successfully!");
+}
